f32eaa596b42677890ff068e78a48ce88b815d40
djzwns <hjjshg@game.hoseo.edu>
2016-10-03 14:21:26 +0900

    리지드바디와 콜라이더를 제거하고 캐릭터 컨트롤러를 사용. 점프 바닥체크와 좌우 움직임 구현

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public float fSpeed = 5f;
+    public float fSlowly = 0.5f;
+    public float fJumpForce = 5f;
+
+    Vector3 vMovement;
+    Vector3 vGravity;
+    CharacterController cController;
+    
+    bool bJumping = false;
+
+    
+	void Awake () {
+        cController = GetComponent<CharacterController>();
+	}
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+            bJumping = true;
+    }
+	
+	void FixedUpdate ()
+    {
+        float horizon = Input.GetAxisRaw("Horizontal");
+
+        // 좌우 이동
+        Move(horizon);
+
+        // 점프
+        Jump();
+
+        // 최종 위치 적용
+        cController.Move(vMovement * Time.deltaTime);
+    }
+
+    void Move(float horizon)
+    {
+        vMovement.Set(horizon, 0f, 0f);
+        
+        vMovement = vMovement.normalized * fSpeed;
+
+        vMovement = transform.TransformDirection(vMovement);
+    }
+
+    void Jump()
+    {
+        // 땅에 있지 않으면 중력작용
+        if (!cController.isGrounded)
+        {
+            vGravity += Physics.gravity * Time.deltaTime;
+        }
+        // 땅에서는 중력 x
+        else
+        {
+            vGravity = Vector3.zero;
+
+            if (bJumping)
+            {
+                vGravity.y = fJumpForce;
+                bJumping = false;
+            }
+        }
+
+        vMovement += vGravity;
+    }
+}
