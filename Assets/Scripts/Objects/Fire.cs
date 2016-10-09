362e2863ceb41da881b871cb849ac7cfff5f2392
djzwns <hjjshg@game.hoseo.edu>
2016-10-09 22:51:16 +0900

    불 스크립트 생성, 함정 스크립트 생성, 불 프리팹 임시용으로 그림판작업.

---
+++ Assets/Scripts/Objects/Fire.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fire : MonoBehaviour {
+    Object obj; // 충돌한 오브젝트 컴포넌트
+
+    void OnTriggerEnter(Collider coll)
+    {
+        if (coll.gameObject.tag == "Object")
+        {
+            obj = coll.GetComponent<Object>();
+
+            // 오브젝트가 불에 영향을 받으면 실행
+            if (EnumFlagAttribute.HasFlag(obj.eInter, Object.EInteraction.Fire))
+            {
+                Destroy(coll.gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Objects/Trap.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trap : MonoBehaviour {
+
+    // 함정 발동 위치
+    public GameObject trapPosition;
+
+    // 발동 될 함정
+    public GameObject trap;
+
+    GameObject tempTrap;
+
+    // 생성된 트랩 삭제용.. 아마 다른 스크립트에서 불러올 듯.
+    void TrapDelete()
+    {
+        Destroy(tempTrap);
+    }
+
+    void OnTriggerEnter(Collider coll)
+    {
+        Debug.Log(coll.gameObject.name);
+        if (coll.gameObject.tag == "Player")
+        {
+            tempTrap = (GameObject) Instantiate(trap, trapPosition.transform.position, Quaternion.identity);
+        }
+    }
+}
