ad6a18ab1e925bf85ccadad4b9199728b7950cd3
djzwns <hjjshg@game.hoseo.edu>
2016-10-15 11:31:22 +0900

    임시 메인화면 생성, 이미지 + 스크립트

---
+++ Assets/Scripts/UI/MainSceneUI.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainSceneUI : MonoBehaviour {
+
+    public void GameStart()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void GameExit()
+    {
+        Application.Quit();
+    }
+}
