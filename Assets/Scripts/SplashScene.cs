6cf074604cc3d96c90c93c695574d5175828597e
djzwns <hjjshg@game.hoseo.edu>
2016-10-29 19:43:50 +0900

    팀로고 추가

---
+++ Assets/Scripts/SplashScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScene : MonoBehaviour {
+
+    public void NextScene()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
