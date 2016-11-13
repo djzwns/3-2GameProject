2ef5ea8553dcf7da14e5fbba06034404e6feb5e0
djzwns <hjjshg@game.hoseo.edu>
2016-11-13 16:15:07 +0900

    문 추가, 아직 문열고 아이템은 없음

---
+++ Assets/Scripts/Objects/Door.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Door : MonoBehaviour {
+    public string sceneName;
+
+    PlayerController player;
+    Image blind;
+
+    void Start()
+    {
+        player = GameObject.Find("Player").GetComponent<PlayerController>();
+        blind = GameObject.Find("blind").GetComponent<Image>();
+    }
+
+    IEnumerator MoveScene()
+    {
+        while (blind.color.a < 1f)
+        {
+            blind.color = new Color(blind.color.r, blind.color.g, blind.color.b, blind.color.a + 0.01f);
+            yield return new WaitForSeconds(0.05f);
+        }
+        SceneManager.LoadScene(sceneName);
+
+    }
+
+    void OnTriggerStay(Collider coll)
+    {
+        if (coll.tag == "Player" && player.bRotating)
+        {
+            StartCoroutine(MoveScene());
+        }
+    }
+}
